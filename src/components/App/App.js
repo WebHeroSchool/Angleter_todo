2cdab1712ca58921bf2c18856f80d2f353927f47
Anastasia Legert <legertlab@gmail.com>
2019-10-03 12:31:20 +0200

    Done task to use CSS modules

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
-import './App.css';
+import styles from './App.module.css';
 
 const App = () => {
   const items = [
@@ -18,8 +18,8 @@ const App = () => {
   ]
   
   
-  return (<div className='wrap'>
-  <h3 className='wrap__title'>Важные дела:</h3>
+  return (<div className={styles.wrap}>
+  <h3 className={styles.title}>Важные дела:</h3>
   <InputItem />
   <ItemList items={items} />
   <Footer count={3} />
