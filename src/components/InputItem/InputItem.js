82ebb90b1c2ad52693cab1d5308b4ce314b389bc
Anastasia Legert <legertlab@gmail.com>
2019-10-03 16:32:40 +0200

    Add component from Material UI

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import styles from './InputItem.module.css';
-const InputItem = () => (<p>InputItem</p>);
+import TextField from '@material-ui/core/TextField';
+
+const InputItem = () => (<div>
+    <TextField 
+        id="outlined-name"
+        label="Name"
+        margin="normal"
+        variant="outlined"
+    />
+</div>);
 
 export default InputItem;
