e8d40c09a3fa8f036cb705150bf7ec59a46f8c9a
Anastasia Legert <legertlab@gmail.com>
2019-10-04 14:23:12 +0200

    Done stylisation

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import classnames from 'classnames';
 import styles from './Item.module.css';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
+
 
 const Item = ( {value, isDone} ) => (<span className={
-    classnames ({
-        [styles.item]: true,
-        [styles.done]: isDone
-    })
-}>
-    <span>{value}</span>
-    <span>
-    <IconButton aria-label='delete'>
-        <DeleteIcon fontSize='small' />
-    </IconButton>
-    </span>
-</span>
-
-)
+            classnames ({
+                [styles.item]: true,
+                [styles.done]: isDone
+            })
+        }>
+    {value}
+</span>)
 
 export default Item;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Item from '../Item/Item';
 import styles from './ItemList.module.css';
 import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const ItemList = ({items}) => (
   <ul className={styles.itemList}>
     {items.map(item => 
-    <li key = {item.value}>
+    <li key = {item.value} className={styles.item}>
       <Checkbox
         defaultChecked
         color='default'
@@ -15,9 +17,15 @@ const ItemList = ({items}) => (
           'aria-label': 'checkbox with default color',
         }}
       />
-      <Item value={item.value} isDone={item.isDone} />
+      <div className={styles.itemText}><Item value={item.value} isDone={item.isDone} /></div>
+      <div>
+        <IconButton aria-label='delete'>
+            <DeleteIcon fontSize='small' />
+        </IconButton>
+      </div>
     </li>)}
   </ul>
 );
 
 export default ItemList;
+
